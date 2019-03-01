69fc5ddaf11796b9173076add17da7a51d6f0e49
Pedro Gómez <pedro.gomez.2019@upb.edu.co>
2019-03-01 10:47:25 -0500

    Se cambió todo el código para empezar a desarrollar la otra parte del ejercicio.

--- mayor.py
+++ mayor.py
@@ -1,13 +1,15 @@
-#Algoritmo que permita determinar si un número entero es mayor a cero
-#Primero vamos a crerar una variable X para que reciba un valor 
-x=eval(input("Bienvenido! Vamos a evaluar el número ingresado para saber si es mayor a cero. Por favor ingresa un número entero: "))
-#Se va a añadir una verificación del número ingresado para ver si es entero o es 0
-if type(x) == float or x == 0:
-    print("El número no es entero o es igual a cero. Gracias por utilizar el programa.")
-else:
-    if x > 0:
-        print ("El número {} es mayor a cero. Gracias por utilizar el programa.".format(x))
-    else:
-        print("El número {} es menor a cero. Gracias por utilzar el programa.".format(x))
+#Algoritmo que lea 10 números y determine cuántos números fueron positivos , cuántos negativos y cuantos fueron cero.
+#Vamos a empezar añadiendo cada uno de los números individualmente
+a1=int(input("Bienvenido! Vamos a analizar 10 números para ver si fueron positivos, negativos y/o cero. Por favor ingrese el primer número: "))
+a2=int(input("Ingrese el segundo número: "))
+a3=int(input("Ingrese el tercer número: "))
+a4=int(input("Ingerse el cuarto número: "))
+a5=int(input("Ingrese el quinto número: "))
+a6=int(input("Ingrese el sexto número: "))
+a7=int(input("Ingrese el séptimo número: "))
+a8=int(input("Ingrese el octavo número: "))
+a9=int(input("Ingrese el noveno número: "))
+a10=int(input("Ingrese el décimo número: "))
+
 # Desarrollado por Pedro Gómez / ID:000396221
         
