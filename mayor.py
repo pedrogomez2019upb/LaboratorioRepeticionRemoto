a4dee5d3dc2b448edbdc9a0b7bce85a3ac69b13d
Pedro Gómez <pedro.gomez.2019@upb.edu.co>
2019-03-01 10:23:06 -0500

    Se hizo el prgrama funcional de manera muy básica, pero se busca mejorarlo.

---
+++ mayor.py
@@ -0,0 +1,10 @@
+#Algoritmo que permita determinar si un número entero es mayor a cero
+#Primero vamos a crerar una variable X para que reciba un valor 
+x=int(input("Bienvenido! Vamos a evaluar el número ingresado para saber si es mayor a cero. Por favor ingresa un número entero: "))
+
+if x > 0:
+    print ("El número {} es mayor a cero. Gracias por utilizar el programa.".format(x))
+else:
+    print ("El número {} es menor a cero. Gracias por utilizar el programa.".format(x))
+# Desarrollado por Pedro Gómez / ID:000396221
+        
