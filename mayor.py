4e616c17c56d314416f9a5932a8e107a9c48ad65
Pedro Gómez <pedro.gomez.2019@upb.edu.co>
2019-03-01 11:21:26 -0500

    Se intentó mejorar el programa mediante permitiendo al usuario ingresar los valores, pero el programa no quiere tirar la cantidad de número, por lo que se va  a proceder a cambiar el código.

--- mayor.py
+++ mayor.py
@@ -1,15 +1,21 @@
 #Algoritmo que lea 10 números y determine cuántos números fueron positivos , cuántos negativos y cuantos fueron cero.
-#Vamos a empezar añadiendo cada uno de los números individualmente
-a1=int(input("Bienvenido! Vamos a analizar 10 números para ver si fueron positivos, negativos y/o cero. Por favor ingrese el primer número: "))
-a2=int(input("Ingrese el segundo número: "))
-a3=int(input("Ingrese el tercer número: "))
-a4=int(input("Ingerse el cuarto número: "))
-a5=int(input("Ingrese el quinto número: "))
-a6=int(input("Ingrese el sexto número: "))
-a7=int(input("Ingrese el séptimo número: "))
-a8=int(input("Ingrese el octavo número: "))
-a9=int(input("Ingrese el noveno número: "))
-a10=int(input("Ingrese el décimo número: "))
+#Vamos a hacer un for para que pueda ingresar la cantidad que quiera evaluar
+NumList = []
+Positive_count = 0
+Negative_count = 0
+#Ingresa el número de números que quiere evaluar
+Number = int(input("Bienvenid@! Vamos a evaluar los números que quieras para ver si son positivos , negativos o igual a cero: Ingresa la cantidad que quieres evaluar: "))
+for i in range(1, Number + 1):
+    value = int(input("Por favor ingresa el valor de número %d : " %i))
+    NumList.append(value)
 
+for j in range(Number):
+    if(NumList[j] >= 0):
+        Positive_count = Positive_count + 1
+    else:
+        Negative_count = Negative_count + 1
+
+print("\nTotal Number of Positive Numbers in this List =  ", Positive_count)
+print("Total Number of Negative Numbers in this List = ", Negative_count)
 # Desarrollado por Pedro Gómez / ID:000396221
         
