888fb6be6b789ba4302662fdf4fcf88b5e6f0704
Pedro Gómez <pedro.gomez.2019@upb.edu.co>
2019-03-01 13:04:38 -0500

    Se editó el programa para que funcione con while.

--- promedio.py
+++ promedio.py
@@ -1,10 +1,3 @@
-#Programa que saque el promedio de n notas
-n = int(input("Bienvenido ! Este programa te ayuda a calcular el promedio de n numeros. ¿Cuántas notas vas a promediar?: "))
-total_suma = 0
-#Ahora vamos a poner una condicional for para que cumpla las condicionales
-for n in range(n):
-    numbers = float(input("Ingresa el valor : "))
-    total_suma += numbers
-promedio = total_suma/n
-print("El promedio de las notas es de {}".format(promedio))
+#Programa que saque el promedio de n notas con la función while
+
 #Creado por Pedro Gómez / ID:000396221
