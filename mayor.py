1a6de89a9988fbc8fab66c3b17ceaf823b04cc42
Pedro Gómez <pedro.gomez.2019@upb.edu.co>
2019-03-01 10:40:41 -0500

    Mi primer commit

--- mayor.py
+++ mayor.py
@@ -1,7 +1,7 @@
 #Algoritmo que permita determinar si un número entero es mayor a cero
 #Primero vamos a crerar una variable X para que reciba un valor 
 x=eval(input("Bienvenido! Vamos a evaluar el número ingresado para saber si es mayor a cero. Por favor ingresa un número entero: "))
-#Se va a añadir un chequeo del número ingresado para ver si es entero o es 0
+#Se va a añadir una verificación del número ingresado para ver si es entero o es 0
 if type(x) == float or x == 0:
     print("El número no es entero o es igual a cero. Gracias por utilizar el programa.")
 else:
