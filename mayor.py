c81f298a0e71798c7a459b4fec04c38b9cb73342
Pedro Gómez <pedro.gomez.2019@upb.edu.co>
2019-03-01 11:21:36 -0500

    Se intentó mejorar el programa mediante permitiendo al usuario ingresar los valores, pero el programa no quiere tirar la cantidad de número, por lo que se va  a proceder a cambiar el código.

--- mayor.py
+++ mayor.py
@@ -1,8 +1,8 @@
 #Algoritmo que lea 10 números y determine cuántos números fueron positivos , cuántos negativos y cuantos fueron cero.
 #Vamos a hacer un for para que pueda ingresar la cantidad que quiera evaluar
 NumList = []
-Positive_count = 0
-Negative_count = 0
+positivos = 0
+negativos = 0
 #Ingresa el número de números que quiere evaluar
 Number = int(input("Bienvenid@! Vamos a evaluar los números que quieras para ver si son positivos , negativos o igual a cero: Ingresa la cantidad que quieres evaluar: "))
 for i in range(1, Number + 1):
@@ -11,11 +11,11 @@ for i in range(1, Number + 1):
 
 for j in range(Number):
     if(NumList[j] >= 0):
-        Positive_count = Positive_count + 1
+        postivos = positivos + 1
     else:
-        Negative_count = Negative_count + 1
+        negativos = negativos + 1
 
-print("\nTotal Number of Positive Numbers in this List =  ", Positive_count)
-print("Total Number of Negative Numbers in this List = ", Negative_count)
+print("\nLos números postivios de la lista fueron =  ", postivos)
+print("Los números negativos de la lista fueron = ", negativos)
 # Desarrollado por Pedro Gómez / ID:000396221
         
