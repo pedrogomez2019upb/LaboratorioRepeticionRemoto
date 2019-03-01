25e430676ef3dd281f92f72a7792bbd061170853
Pedro Gómez <pedro.gomez.2019@upb.edu.co>
2019-03-01 11:32:40 -0500

    Se intenta otra vez, pero no da el resultado.

--- mayor.py
+++ mayor.py
@@ -1,21 +1,37 @@
 #Algoritmo que lea 10 números y determine cuántos números fueron positivos , cuántos negativos y cuantos fueron cero.
-#Vamos a hacer un for para que pueda ingresar la cantidad que quiera evaluar
+#Vamos a definir tres contadores ; uno para los positivos , uno para los negativos y finalmente otro para los ceros.
+def count_positivos(NumList):
+    positivos = 0
+    for j in range(Number):
+        if(NumList[j] >= 0):
+            positivos = positivos + 1
+    return positivos
+
+def count_negativos(NumList):
+    negativos = 0
+    for j in range(Number):
+        if(NumList[j] % 2 != 0):
+            negativos = negativos + 1
+    return negativos
+
+def count_ceros(NumList):
+    ceros = 0
+    for j in range(Number):
+        if(NumList[j] == 0):
+            ceros = ceros + 1
+    return ceros
+
 NumList = []
-positivos = 0
-negativos = 0
-#Ingresa el número de números que quiere evaluar
-Number = int(input("Bienvenid@! Vamos a evaluar los números que quieras para ver si son positivos , negativos o igual a cero: Ingresa la cantidad que quieres evaluar: "))
+Number = int(input("Please enter the Total Number of List Elements: "))
 for i in range(1, Number + 1):
-    value = int(input("Por favor ingresa el valor de número %d : " %i))
+    value = int(input("Please enter the Value of %d Element : " %i))
     NumList.append(value)
 
-for j in range(Number):
-    if(NumList[j] >= 0):
-        postivos = positivos + 1
-    else:
-        negativos = negativos + 1
-
-print("\nLos números postivios de la lista fueron =  ", postivos)
-print("Los números negativos de la lista fueron = ", negativos)
+positivos_cnt = count_positivos(NumList)
+negativos_cnt = count_negativos(NumList)
+ceros_cnt = count_ceros(NumList)
+print("\nTotal Number of Positive Numbers in this List =  ", positivos_cnt)
+print("Total Number of Negative Numbers in this List = ", negativos_cnt)
+print ("El número total de ceros en la lista es {}".format(ceros_cnt))
 # Desarrollado por Pedro Gómez / ID:000396221
         
