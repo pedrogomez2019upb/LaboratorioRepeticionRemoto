5b90d36324c37df24443407bbbc2fc827a919c4d
Pedro Gómez <pedro.gomez.2019@upb.edu.co>
2019-03-01 11:42:19 -0500

    Se hizo la programación funcional y en este momento está compilando .

--- mayor.py
+++ mayor.py
@@ -1,37 +1,37 @@
 #Algoritmo que lea 10 números y determine cuántos números fueron positivos , cuántos negativos y cuantos fueron cero.
 #Vamos a definir tres contadores ; uno para los positivos , uno para los negativos y finalmente otro para los ceros.
-def count_positivos(NumList):
-    positivos = 0
+def count_Positive(NumList):
+    Positive_count = 0
     for j in range(Number):
-        if(NumList[j] >= 0):
-            positivos = positivos + 1
-    return positivos
+        if(NumList[j] > 0):
+            Positive_count = Positive_count + 1
+    return Positive_count
 
-def count_negativos(NumList):
-    negativos = 0
+def count_Negative(NumList):
+    Negative_count = 0
     for j in range(Number):
         if(NumList[j] % 2 != 0):
-            negativos = negativos + 1
-    return negativos
+            Negative_count = Negative_count + 1
+    return Negative_count
 
-def count_ceros(NumList):
-    ceros = 0
+def count_zeroes(NumList):
+    zeroes_count = 0
     for j in range(Number):
-        if(NumList[j] == 0):
-            ceros = ceros + 1
-    return ceros
+        if (NumList[j] == 0):
+            zeroes_count = zeroes_count + 1
+    return zeroes_count
 
 NumList = []
-Number = int(input("Please enter the Total Number of List Elements: "))
+Number = int(input("Bienvenido! Por favor Ingrese el número de números que quieres ingresar para evaluar: "))
 for i in range(1, Number + 1):
-    value = int(input("Please enter the Value of %d Element : " %i))
+    value = int(input("Por favor ingresa el valor del numero %d : " %i))
     NumList.append(value)
 
-positivos_cnt = count_positivos(NumList)
-negativos_cnt = count_negativos(NumList)
-ceros_cnt = count_ceros(NumList)
-print("\nTotal Number of Positive Numbers in this List =  ", positivos_cnt)
-print("Total Number of Negative Numbers in this List = ", negativos_cnt)
-print ("El número total de ceros en la lista es {}".format(ceros_cnt))
+Positive_cnt = count_Positive(NumList)
+Negative_cnt = count_Negative(NumList)
+zeroes_cnt = count_zeroes(NumList)
+print("\nEl número total de números positivos es :  ", Positive_cnt)
+print("El número total de números negativos es : ", Negative_cnt)
+print("El número total de zeros es: ",zeroes_cnt)
 # Desarrollado por Pedro Gómez / ID:000396221
         
